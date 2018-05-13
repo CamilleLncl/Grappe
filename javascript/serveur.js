581cb7a9cc9c41fb54796cfddab09ece5f33393d
CamilleLncln <camille.lancelin@epfedu.fr>
2018-05-13 19:50:18 +0200

    Connexion add

--- javascript/serveur.js
+++ javascript/serveur.js
@@ -2,7 +2,7 @@ var http = require('http'); //appel bibliotheque http de node js
 
 var server = http.createServer(function(req, res) { 
   res.writeHead(200);
-  res.end(index.html);
+  res.end('Salut tout le monde !');
 });
 server.listen(9090);
 
