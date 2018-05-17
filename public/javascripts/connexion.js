4275550d96c40ef50f05aa2e8d23fdf2a1d32f02
CamilleLncln <camille.lancelin@epfedu.fr>
2018-05-17 22:41:39 +0200

    Add connexion + accueil

---
+++ public/javascripts/connexion.js
@@ -0,0 +1,29 @@
+/*(function(){
+	angular.module("store",[]);
+})();
+
+function {
+
+	var identifiant = document.getElementyId("identifiant").value; 
+	var mdp = document.getElementyId("mdp").value;
+
+if ( identifiant == "Camille" && mdp == "123"){
+alert ("Login successfully");
+window.location = "/accueil.ejs"; // Redirecting to other page.
+return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+};
+
+
+*/
