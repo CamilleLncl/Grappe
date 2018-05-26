2ca5ab191e3c2ef869ef92721b0f8ffed4366241
CamilleLncln <camille.lancelin@epfedu.fr>
2018-05-26 16:59:33 +0200

    Finish connextion add 2 page and stat module (charts)

--- app.js
+++ app.js
@@ -5,17 +5,62 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cons = require('consolidate');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var accueilRouter = require('./routes/accueil');
-
+var statRouter = require('./routes/statistiques');
+var gestionRouter = require('./routes/gestion');
 
 var app = express();
+/*var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'nl1-ss1.a2hosting.com',
+  user     : 'grappeah_admin',
+  password : 'Grappehosting42@',
+  database : 'grappeah_BDD_Tempo'
+});
+connection.connect(function(err){
+  console.log(err);
+if(!err) {
+    console.log("Database is connected ... ");
+} else {
+    console.log("Error connecting database ... ");
+}
+});*/
 
 // view engine setup
+/*app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');*/
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'nl1-ss1.a2hosting.com',
+  user     : 'grappeah_admin',
+  password : 'k7d8X3A4nj',
+  database : 'grappeah_BDD_Tempo'
+});
+
+connection.connect(function(err){
+  console.log(err);
+if(!err) {
+    console.log("Database is connected ... ");
+} else {
+    console.log("Error connecting database ... ");
+}
+});
+
+/*connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});*/
+
+connection.end();
+app.engine('html', cons.swig)
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -26,7 +71,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/', accueilRouter);
-
+app.use('/statistiques', statRouter);
+app.use('/gestion', gestionRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/accueil.js
+++ routes/accueil.js
@@ -3,10 +3,38 @@ var bodyParser = require("body-parser");
 var router = express.Router();
 var app = express();
 
-router.post('/', function(req, res, next) {
-	
-  res.render('accueil');
-	
-});
 
+
+
+
+router.post('/', function(req, res, next) {
+	/*con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `created`, `modified`) VALUES (NULL, 'Luc', 'Bobby', 'luc.bobby@epf.de', 'lulu', 'NOW()', '');";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+});*/
+	
+//function validate (req, res, next){
+  console.log('GOOD JOB');
+var identifiant = req.body.username;
+var mdp = req.body.password;
+
+console.log("id : "+identifiant + " mdp : "+mdp);
+
+if ( identifiant == "Camille" && mdp == "123"){
+//alert ("Login successfully");
+ res.render('accueil');
+}
+else{
+  //alert("Mauvais identifiant ou mot de passe");
+
+  console.log("mieux");
+  res.render('fail');
+}
+});
+ 
 module.exports = router;
